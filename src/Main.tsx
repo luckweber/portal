ea5a785e698b3bcdfc7232a5973e672b3f754166
Thiago Pereira <thiago.pereira@agencian1.com.br>
2019-03-27 00:01:12 -0300

    Create Component Main

---
+++ src/Main.tsx
@@ -0,0 +1,15 @@
+import  React, { Fragment, SFC, MouseEvent } from 'react'
+
+//https://itnext.io/react-component-class-vs-stateless-component-e3797c7d23ab
+//https://levelup.gitconnected.com/ultimate-react-component-patterns-with-typescript-2-8-82990c516935
+//https://medium.com/@ethan_ikt/react-stateless-functional-component-with-typescript-ce5043466011
+//https://medium.com/@oieduardorabelo/typescript-o-guia-definitivo-1a63b04259cc
+
+
+
+export const Main:SFC = () => (
+    <Fragment>
+        ddd
+    </Fragment>
+)
+export default Main;
